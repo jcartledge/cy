52d5bd17f6772de8c551e66d12367c9a3a252e78
James Cartledge <jcartledge@gmail.com>
2012-03-17 12:23:28 +1100

    Created application using Refinery CMS 2.0.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoyYiontis::Application.config.secret_token = 'c4aa8631944f419610dc604a9c4e3edd0da59472edfee5b52bfdac85c567fb88db8cafef4d8aed28cf614ee12fc71cf75845a4f460ad9f61307fdc67f866aa8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CoyYiontis::Application.config.session_store :cookie_store, key: '_coy_yiontis_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CoyYiontis::Application.config.session_store :active_record_store
